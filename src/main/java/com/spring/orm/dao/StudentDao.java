b8c36e67a3d9c3ba769771e206908e520a851f5a
PranavRM5 <pranavmahajan510@gmail.com>
2025-01-27 15:21:58 +0530

    Spring

--- src/main/java/com/spring/orm/dao/StudentDao.java
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,8 +1,13 @@
 package com.spring.orm.dao;
 
+import java.util.List;
+
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import com.spring.orm.entities.Student;
+
+
 import org.springframework.transaction.annotation.Transactional;
+
 
 public class StudentDao {
 
@@ -14,7 +19,31 @@ public class StudentDao {
         Integer id = (Integer) this.hibernateTemplate.save(student);
         return id;
     }
+    
+    //get single data(object)
+    public Student getStudent(int studentId) {
+    	Student student = this.hibernateTemplate.get(Student.class, studentId);
+    	return student;
+    }
+    
+    //get All data(All row)
+    public List<Student> getAllStudents(){
+    	List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+    	return students;
+    }
 
+    //deleting the data
+    public void deleteStudent(int studentId) {
+    	Student student = this.hibernateTemplate.get(Student.class, studentId);
+    	this.hibernateTemplate.delete(student);
+    }
+    
+    //updating the data
+    @Transactional
+    public void updateStudent(Student student) {
+    	this.hibernateTemplate.update(student);
+    }
+    
     public HibernateTemplate getHibernateTemplate() {
         return hibernateTemplate;
     }
